3bc61e1979da0be60692af1a89e08bf5254414dd
artem <ghtp7397@gmail.com>
2025-11-29 10:23:41 +0300

    ProductRepository

--- Inno_Shop/src/InnoShop.Products/InnoShop.Products.Application/Interfaces/Repositories/IProductRepository.cs
+++ Inno_Shop/src/InnoShop.Products/InnoShop.Products.Application/Interfaces/Repositories/IProductRepository.cs
@@ -5,10 +5,10 @@ namespace InnoShop.Products.Application.Interfaces.Repositories;
 public interface IProductRepository
 {
     Task<Product?> GetByIdAsync(Guid id, bool includeDeleted = false);
-    Task<List<Product>> GetAllAsync(bool includeDeleted = false);
-    Task<List<Product>> GetByUserIdAsync(Guid userId, bool includeDeleted = false);
+    Task<IEnumerable<Product>> GetAllAsync(bool includeDeleted = false);
+    Task<IEnumerable<Product>> GetByUserIdAsync(Guid userId, bool includeDeleted = false);
 
-    Task<(List<Product> Products, int TotalCount)> SearchAsync(
+    Task<(IEnumerable<Product> Products, int TotalCount)> SearchAsync(
         string? searchTerm,
         decimal? minPrice,
         decimal? maxPrice,
